fd0c9ad4594da18a7690dc50205430d82f21398e
Joseph Escobedo <joe_escob1023@live.com>
2019-07-17 21:53:05 -0700

    init

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const jumbotron = document.querySelector(".jumbotron");
+let color1 = document.querySelector(".color1");
+
+let color2 = document.querySelector(".color2");
+let color3 = document.querySelector(".color3");
+const button = document.querySelector(".btn");
+
+function getRandomColor() {
+  let letters = "0123456789ACBDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function colorChange() {
+  jumbotron.style.backgroundColor = getRandomColor();
+}
+setInterval(colorChange, 1000);
+
+button.addEventListener("click", e => {
+  //if the colors value is greater than 255, error message
+  e.preventDefault();
+});
