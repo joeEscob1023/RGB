e65424ff9f06225d32113fe09f5bb14b6ad66f2b
Joseph Escobedo <joe_escob1023@live.com>
2019-07-20 16:10:27 -0700

    made a comment on how the loop should act

--- js/app.js
+++ js/app.js
@@ -24,10 +24,9 @@ button.addEventListener("click", e => {
   colors.forEach(function(color) {
     if (color.value <= 255 && color.value >= 0 && color.value !== NaN) {
       color.style.border = "green 3px solid";
-      let r = color.value[0];
-      let g = color.value[1];
-      let b = color.value[2];
-      console.log(`this is my rgb: rgb(${r}, ${g}, ${b})`);
+      //loop needs to return something like rgb(54, 98, 245)
+
+      console.log(color.value, color.value, color.value);
     } else {
       color.style.border = "red 3px solid";
       console.log("fail");
