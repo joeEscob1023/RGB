aeedf5aa844a9f2bb4af9b70296b633b76872242
Joseph Escobedo <joe_escob1023@live.com>
2019-07-20 14:45:34 -0700

    need to get value for each color input

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const jumbotron = document.querySelector(".jumbotron");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let color3 = document.querySelector(".color3");
+const body = document.querySelector("body");
+let colors = document.querySelectorAll(".colors");
+console.log(colors);
+
 const button = document.querySelector(".btn");
 
 function getRandomColor() {
@@ -20,5 +21,17 @@ setInterval(colorChange, 1000);
 
 button.addEventListener("click", e => {
   //if the colors value is greater than 255, error message
+  colors.forEach(function(color) {
+    if (color.value <= 255 && color.value >= 0 && color.value !== NaN) {
+      color.style.border = "green 3px solid";
+      let r = color.value[0];
+      let g = color.value[1];
+      let b = color.value[2];
+      console.log(`this is my rgb: rgb(${r}, ${g}, ${b})`);
+    } else {
+      color.style.border = "red 3px solid";
+      console.log("fail");
+    }
+  });
   e.preventDefault();
 });
