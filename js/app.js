f17541e7e2b6a4149cb49a3608a396c16dfa3364
Joseph Escobedo <joe_escob1023@live.com>
2019-07-21 19:28:27 -0700

    added an empty array and push the color values into it so i could use each index of the array as the rgb value

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const jumbotron = document.querySelector(".jumbotron");
 const body = document.querySelector("body");
 let colors = document.querySelectorAll(".colors");
-console.log(colors);
 
 const button = document.querySelector(".btn");
 
+//This function just sets the jumbotron change colors randomly
 function getRandomColor() {
   let letters = "0123456789ACBDEF";
   let color = "#";
@@ -14,19 +14,25 @@ function getRandomColor() {
   return color;
 }
 
+//Allows the jumbotron to change colors randomly. color / per second
 function colorChange() {
   jumbotron.style.backgroundColor = getRandomColor();
 }
 setInterval(colorChange, 1000);
 
+/*
+  When the user adds a number value to the the inputs, it will change the rgb color on submit
+*/
 button.addEventListener("click", e => {
   //if the colors value is greater than 255, error message
+  let rgbVals = [];
   colors.forEach(function(color) {
     if (color.value <= 255 && color.value >= 0 && color.value !== NaN) {
-      color.style.border = "green 3px solid";
-      //loop needs to return something like rgb(54, 98, 245)
+      rgbvaleus = rgbVals.push(color.value);
 
-      console.log(color.value, color.value, color.value);
+      body.style.backgroundColor = `rgb(${rgbVals[0]}, ${rgbVals[1]}, ${
+        rgbVals[2]
+      })`;
     } else {
       color.style.border = "red 3px solid";
       console.log("fail");
